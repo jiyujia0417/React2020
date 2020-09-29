28b33ad2e46ea760903479bb56f6c3d31cdc12ce
jiyujia0417 <jiyujia0417@163.com>
2020-09-29 19:35:12 +0800

    雪梨1第一次提交

---
+++ xueli1/build/precache-manifest.a6ecf788c9383f6aab2e31aad5020b31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a7662162113df4fe11fe979316e6ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd2574f1652b285f5c2b",
+    "url": "/static/js/2.f8e35e53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8e35e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5280fdac544a8faa47e5",
+    "url": "/static/js/main.f3b34249.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
