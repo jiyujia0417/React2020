324553316dd448751ac43fa9c8e6b2b02b175c88
jiyujia0417 <jiyujia0417@163.com>
2020-10-19 20:10:50 +0800

    homework3 first commit

---
+++ xueli3/build/precache-manifest.1dac48b76b78ce852e469665a71ee56c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93cfb68b4fcbab3e5f8966cdeb8bad09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25992a847d0ad341e651",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "019965d54bbb1b796765",
+    "url": "/static/css/main.8e49c38a.chunk.css"
+  },
+  {
+    "revision": "25992a847d0ad341e651",
+    "url": "/static/js/2.0c68a255.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.0c68a255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "019965d54bbb1b796765",
+    "url": "/static/js/main.96ffcfc3.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
