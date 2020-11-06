f97b47346c9adb1388070a678257f21b895fec73
jiyujia0417 <jiyujia0417@163.com>
2020-11-06 13:25:44 +0800

    finish 5

---
+++ xueli5/build/precache-manifest.e34833171bfd733d5298a60463ae7882.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de564007e8ea81e6f7057acbe4a3d94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61dc02fd83668c76c8b1",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "bf7aa7bdc0c0f843f0fd",
+    "url": "/static/css/main.7176bee7.chunk.css"
+  },
+  {
+    "revision": "61dc02fd83668c76c8b1",
+    "url": "/static/js/2.280b1d46.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.280b1d46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf7aa7bdc0c0f843f0fd",
+    "url": "/static/js/main.09464f08.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
